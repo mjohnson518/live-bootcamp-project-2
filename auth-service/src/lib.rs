60ac907349610152e078c28a09e2d41163875961
mjohnson518 <marcjohnson518@pm.me>
2024-10-06 16:50:52 +0100

    [Sprint 1][Task 4] complete // [Sprint 1][Task 5] W-I-P

---
+++ auth-service/src/lib.rs
@@ -0,0 +1,55 @@
+use axum::{Router, routing::post, http::StatusCode, response::IntoResponse};
+use axum::serve::Serve;
+use tower_http::services::ServeDir;
+use std::error::Error;
+
+pub struct Application {
+    server: Serve<Router, Router>,
+    pub address: String,
+}
+
+impl Application {
+    pub async fn build(address: &str) -> Result<Self, Box<dyn Error>> {
+        let router = Router::new()
+            .nest_service("/", ServeDir::new("assets"))
+            .route("/signup", post(signup))
+            .route("/login", post(login))
+            .route("/logout", post(logout))
+            .route("/verify-2fa", post(verify_2fa))
+            .route("/verify-token", post(verify_token));
+
+        let listener = tokio::net::TcpListener::bind(address).await?;
+        let address = listener.local_addr()?.to_string();
+        let server = axum::serve(listener, router);
+
+        Ok(Self {
+            server,
+            address,
+        })
+    }
+
+    pub async fn run(self) -> Result<(), std::io::Error> {
+        println!("listening on {}", &self.address);
+        self.server.await
+    }
+}
+
+async fn signup() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn login() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn logout() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn verify_2fa() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn verify_token() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
---
+++ auth-service/tests/api/routes.rs
@@ -0,0 +1,46 @@
+use crate::helpers::TestApp;
+
+#[tokio::test]
+async fn root_returns_auth_ui() {
+    let app = TestApp::new().await;
+
+    let response = app.get_root().await;
+
+    assert_eq!(response.status().as_u16(), 200);
+    assert_eq!(response.headers().get("content-type").unwrap(), "text/html");
+}
+
+#[tokio::test]
+async fn signup_returns_200() {
+    let app = TestApp::new().await;
+    let response = app.signup().await;
+    assert_eq!(response.status().as_u16(), 200);
+}
+
+#[tokio::test]
+async fn login_returns_200() {
+    let app = TestApp::new().await;
+    let response = app.login().await;
+    assert_eq!(response.status().as_u16(), 200);
+}
+
+#[tokio::test]
+async fn logout_returns_200() {
+    let app = TestApp::new().await;
+    let response = app.logout().await;
+    assert_eq!(response.status().as_u16(), 200);
+}
+
+#[tokio::test]
+async fn verify_2fa_returns_200() {
+    let app = TestApp::new().await;
+    let response = app.verify_2fa().await;
+    assert_eq!(response.status().as_u16(), 200);
+}
+
+#[tokio::test]
+async fn verify_token_returns_200() {
+    let app = TestApp::new().await;
+    let response = app.verify_token().await;
+    assert_eq!(response.status().as_u16(), 200);
+}
