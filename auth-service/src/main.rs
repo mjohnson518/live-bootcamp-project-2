d38e50e5319a61d1d178bb61d6e9dba06dabac64
mjohnson518 <marcjohnson518@pm.me>
2024-10-06 14:17:32 +0100

    [Sprint 1][Task 2]: edit

--- auth-service/src/main.rs
+++ auth-service/src/main.rs
@@ -18,5 +18,5 @@ async fn main() {
 
 async fn hello_handler() -> Html<&'static str> {
     // TODO: Update this to a custom message!
-    Html("<h1>What's up, World!?</h1>")
+    Html("<h1>What's up, Marc!?</h1>")
 }
